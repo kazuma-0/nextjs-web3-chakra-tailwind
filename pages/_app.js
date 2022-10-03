c952756a2c6d55092efac08cd883367f68d8dede
kazuma-0 <as8644459@gmail.com>
2022-10-03 20:16:24 +0530

    Add tailwindcss, wallet provider and chakra provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
-
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import "../styles/globals.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { useMemo } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <ChakraProvider>
+            <Component {...pageProps}></Component>
+          </ChakraProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
